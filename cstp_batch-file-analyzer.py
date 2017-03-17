1e69a5dd74fd7169465552f2381019cf5cf9aba9
Phillip Marks <phill.marks@yahoo.co.uk>
2017-03-17 18:51:02 +0000

    Analyzes .cif files rather than .castep

--- cstp_batch-file-analyzer.py
+++ cstp_batch-file-analyzer.py
@@ -1,42 +1,39 @@
+#note: Li--N Code is not robust enough and needs commenting
+
 import pandas as pd
 import numpy as np
 import linecache
 
-def filegrabber(angle):
-    castepf = 'GOPT_L-BFGS_LiH33_11LAY_750eV_kpn551_wFixedBulk_44AngBox_SymmGen_w2NH3at_4AngfromSurf_%sdeg.castep' % angle
+#unit cell
+ab = 12.0251939
+c  = 44.0420257
 
-    #locates line of geom1 in castepfile
-    with open(castepf, 'r') as myfile:
-        for (i, line) in enumerate(myfile):
-            if 'Total number of ions in cell =' in line:
-                #returns last word in the line
-                no_of_ions = line.split()[-1]
-            #locates lines in castep file of geo
-            if 'Final Configuration' in line:
-                lineofgeom1 = i + 12
-
-    #https://www.safaribooksonline.com/library/view/python-cookbook-2nd/0596007973/ch02s05.html
-    datalist=[]
-
-    for m in range (lineofgeom1,lineofgeom1 + int(no_of_ions)):
-        theline = linecache.getline(castepf, m)
-        theline = theline.split() #comma seperate
-        theline.pop(0) ,theline.pop(-1) #removes 'x' from beginning and end
-        datalist.append(theline)
+#poor solution which needs improvement
+def filegrabberCIF(angle):
+    #use castep file not cif
+    cifout = 'GOPT_L-BFGS_LiH33_11LAY_750eV_kpn551_wFixedBulk_44AngBox_SymmGen_w2NH3at_4AngfromSurf_%sdeg-out.cif' % angle
     
-    #convert to np and remove first two cols
-    data = np.array(datalist)
-    data = data[:,2:]
-    data = data.astype(float)
+    linecounter = 0
+   
+    with open(cifout, 'r') as myfile:
+        for (line_no, line) in enumerate(myfile):
+
+            #finds line where coordinates start
+            if '_atom_site_occupancy' in line:
+                first_line = line_no + 2
+            linecounter += 1
+        
+    #store lines in rows
+    rows = []
+
+    for line_no in range (first_line,linecounter+1):
+        grabline = linecache.getline(cifout, line_no)
+        grabline = grabline.split() #comma seperate
+        rows.append(grabline)
     
-    #convert to absolute positions
-    #has to be at this stage to calc distance between atoms
-    #can't calculate dist between atoms in frac. coords
-    ab = 12.0251939
-    c  = 44.0420257 
-    data[:,:2] *=ab
-    data[:,2] *=c
-    return data
+    #convert to np
+    rows = np.array(rows)
+    return rows[:,:-2]
 
 #default angles, angles must be entered as createtable(['015','086']) etc.
 def createtable(angles=['000','015','030','045','060',
@@ -46,48 +43,84 @@ def createtable(angles=['000','015','030','045','060',
    
     #loops over filegrabber() for every file
     for item in range(len(angles)):
-        #setup blank table to populate
-        table = np.zeros((1,8))
+                
+        current_file = filegrabberCIF(angles[item])
         
-        current_file = filegrabber(angles[item])
+        #make blank np arrays
+        AmmoniasH = np.zeros(0)
+        AmmoniasN = np.zeros(0)
 
-        #pass current_file to NH bond calculator
-        AmmoniaBonds(current_file)
-        
-        #pass to NLi length
-        NLi = DistNLi(current_file)
-        print NLi
-               
+        #finds H in Ammonia
+        for H_idx in range(1,7):
+            Hrow = np.where(current_file[:,0]=='H%d' % H_idx)
+            AmmoniasH = np.append(AmmoniasH, current_file[Hrow])
 
-        #output table as txtfile
+        for N_idx in range(1,3):
+            Nrow = np.where(current_file[:,0]=='N%d' % N_idx)
+            AmmoniasN = np.append(AmmoniasN, current_file[Nrow])
+        
+        #Lithium atom below N
+        LiBelowAmmonia = current_file[70]
+        print LiBelowAmmonia
 
-def AmmoniaBonds(current_file):
-    #grab xyz of N from current_file
-    for N_rowno in range(1,3):
-        N_atoms = current_file[-(N_rowno)]
+        #reshape
+        AmmoniasH = np.reshape(AmmoniasH,(-1,4)) 
+        AmmoniasN = np.reshape(AmmoniasN,(-1,4))
+        LiBelowAmmonia = np.reshape(LiBelowAmmonia,(-1,4))
+
+        #remove indx and convert to float
+        AmmoniasH = AmmoniasH[:,1:].astype(float)
+        AmmoniasN = AmmoniasN[:,1:].astype(float) 
+        LiBelowAmmonia = LiBelowAmmonia[:,1:].astype(float)
+
+        #LiCode
+        #check Li is the correct one!
+        print LiBelowAmmonia[0,2]
+        print AmmoniasN[0,2]
+        #if surf Li and N are greater than ~13Ang apart 
+        if abs(LiBelowAmmonia[0,2]-AmmoniasN[0,2]) > 0.3:
+            LiBelowAmmonia = 1 - LiBelowAmmonia
+        print LiBelowAmmonia
+
         
-        #poor solution, update in later version
-        if N_rowno == 1:
-            Counter = 3
-        else:
-            Counter = 0
+        #convert fraccoord to absolute positions
+        AmmoniasH[:,:2] *= ab
+        AmmoniasN[:,:2] *= ab
+        LiBelowAmmonia[:,:2] *=ab
+
+        AmmoniasH[:,2] *= c
+        AmmoniasN[:,2] *= c
+        LiBelowAmmonia[:,2] *=c
+
+
+        #calc num_rows
+        num_Hrows = np.shape(AmmoniasH)[0]
+        num_Nrows = np.shape(AmmoniasN)[0]
+        num_Lirows = np.shape(LiBelowAmmonia)[0]
+
+        #SYMMETRYCHECKER
+        if num_Hrows >3 or num_Nrows >1 or num_Lirows > 1:
+            return 'InvSymm Failed'
+        #SYMMETRYCHECKER
         
-        #grab 3H atoms associated with N
-        H_atoms = current_file[0+Counter:3+Counter]
+        #elements coordinates to functions, comment out as appropriate
+        AmmoniaBL(AmmoniasN,AmmoniasH,num_Hrows)    
+        LiN_BL(AmmoniasN, LiBelowAmmonia) 
+        #LiH_BL(AmmoniasH, LiBelowAmmonia)
+
+#Ammonia Bond Lengths
+def AmmoniaBL(AmmoniasN,AmmoniasH,num_Hrows):
         
-        #calculate dist between atoms
-        d = np.sqrt(np.sum( ((H_atoms - N_atoms)**2)  ,axis=1))
+    #grab coordinates of each ammonia in turn
+    for rows in range(num_Hrows):        
+        #d is dist btween atoms
+        d = np.sqrt(np.sum( ((AmmoniasH[rows] - AmmoniasN)**2)  ,axis=1))
+        print 'N--H%d = ' % (rows +1)
         print d
 
-def DistNLi(current_file):
-    #Specific xyz coordinates
-    N_Bottom = current_file[-1]
-    Li_Bottom = current_file[219]    
-    N_Top = current_file[-2]
-    Li_Top =  current_file[227]
-
-    #calculate distance
-    NLi_Top = np.sqrt(np.sum( ((N_Top - Li_Top)**2)))
-    NLi_Bottom = np.sqrt(np.sum( ((N_Bottom - Li_Bottom)**2)))
+def LiN_BL(AmmoniasN, LiBelowAmmonia):
+ 
+    LiN_BondLength = np.sqrt(np.sum( ((AmmoniasN - LiBelowAmmonia)**2)))
     
-    return [NLi_Top, NLi_Bottom]
+    print 'Li--N='
+    print LiN_BondLength
